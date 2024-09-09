4c41c07076dcebfaae843c54800d08497bf91b43
Flaviota <flaviota.dev@gmail.com>
2024-09-09 20:35:13 -0300

    adição de header,styles e implementação jquery

---
+++ src/javascript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    })
+
+
+});
